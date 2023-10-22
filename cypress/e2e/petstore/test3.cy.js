24d8b71da5ebf70efcf0bca36b15efaced31a521
Emiliano Gómez <84544548+egmunist@users.noreply.github.com>
2023-10-22 20:40:34 -0300

    fix 2

--- cypress/e2e/petstore/test3.cy.js
+++ cypress/e2e/petstore/test3.cy.js
@@ -1,9 +1,37 @@
 import 'cypress-plugin-api'
 
-describe('White label - Create a transaction ', () => {
+describe('3 ', () => {
 
- 
-    it("delete user 3", () => {
+    it('Petstore Create user 3', () => {
+       
+       
+        const postData = {
+            "id": 78799797,
+            "username": "testbo21",
+            "firstName": "test",
+            "lastName": "run",
+            "email": "testqapet1223@yopmail.com",
+            "password": "test123",
+            "phone": "54545454545",
+            "userStatus": 1
+          }
+          const headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
+          }
+    
+          cy.api({
+            method: 'POST',
+            url: 'https://petstore.swagger.io/v2/user',
+            body: postData,
+            headers: headers
+          })
+          .then((response) => {
+            expect(response.status).to.eq(200)
+          })
+    });
+
+    it("delete user", () => {
 
  
         cy.api({
